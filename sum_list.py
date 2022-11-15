fd5b57b2d3295a1d8d5ac3d43af831b582452cdb
Marco Cartago <115221586+marco-cartago@users.noreply.github.com>
2022-11-15 12:51:12 +0000

    Aggiunto il file di esercizi della seconda lezione

---
+++ sum_list.py
@@ -0,0 +1,9 @@
+# Scrittura di una funzione che prende 
+# in argomento una lista e ritorna la
+# somma degli elementi della lista
+
+def sum_list(lista):
+    total = 0
+    for element in list: 
+        total += element
+    return total
